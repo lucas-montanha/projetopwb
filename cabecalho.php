d94dbed4ee716e385ccca4ae6ca0ef828c1e3b0c
Laboratorio <laboratorio@escola.local>
2026-08-19 10:46:38 -0300

    php e botao strap

--- cabecalho.php
+++ cabecalho.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/reset.css" />
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
@@ -43,4 +44,4 @@
     </div>
   </div>
 </nav>
-    <div class="container">
+    <div class="container h-75 d-flex align-items-center" style="boder:1px solid #ff0000">
--- index.php
+++ index.php
@@ -1,28 +1,56 @@
 <?php include "cabecalho.php"?>
+
+<?php
+if(isset($_POST["login"])) {
+    
+}
+if(empty($_POST["login"])){
+    $mensagemErro = "Preencha os campos de login";
+}
+    //Procedimento para banco de dados
+
+?>
 <style>
-    div{
-        border:1px solid #11bddb;
-    }
+   
 </style>
-<div class="row">
+<div class="row w-100">
     <div class="col-md-4"></div>
     
     <div class="col-md-4"><!-- coluna do meio-->
+
         <div class="card">
             <div class="card-body">
-            <form action="" method="post">
+
+            <?php
+                if(isset($mensagemErro) && !empty($mensagemErro) ){
+    //Procedimento para banco de dados
+            
+            echo '<div class="alert alert-danger">';
+            echo $mensagemErro;
+            echo '</div>';
+            
+                }
+            ?>
+            <form action="index.php" method="post">
             <label for="login">Username</label>
             <input class="form-control" type="text" name="login" id="login"/>
 
             <label for="senha">Senha</label>
             <input class="form-control" type="password" name="senha" id="senha" />
-            <div class="row">
-                <div class="col-md-6"></div>
-                <div class="col-md-6"></div>
+            <div class="row mt-3">
+                <div class="col-md-6">
+                    <button type="submit" class="btn btn-primary">
+                        Entrar
+                    </button></div>
+                    
+                <div class="col-md-6 align-item-center d-flex justify-content-end">
+                    <input type="checkbox" class= "form-check-input mx=2" value="senha"/> Salvar a Senha
+                </div>
             </div>
     </form>
         </div>
             </div>
+        </div> 
 
     <div class="col-md-4"></div>
 </div>
