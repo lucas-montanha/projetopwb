7e4974983c8b8fa70e1c58d191c10ba878adfcd8
Laboratorio <laboratorio@escola.local>
2026-09-16 11:32:37 -0300

    feito de novo

--- cabecalho.php
+++ cabecalho.php
@@ -27,7 +27,7 @@
                         <a class="nav-link" href="./instalar.php">Instalar Banco</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./cliente.php">Cliente</a>
+                        <a class="nav-link" href="./clientes.php">Cliente</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
