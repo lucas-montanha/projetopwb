289786e401766ee3ab092c95655cc3544f35f664
Laboratorio <laboratorio@escola.local>
2026-09-16 11:31:20 -0300

    feito

--- cabecalho.php
+++ cabecalho.php
@@ -1,38 +1,55 @@
-<?php
-$paginaAtual = basename($_SERVER['PHP_SELF']);
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ProjetoPWB</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="css/reset.css" />
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/reset.css"/>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">ProjetoPWB</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link <?php echo $paginaAtual == 'index.php' ? 'active' : ''; ?>" href="./index.php">
-            Home
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo in_array($paginaAtual, ['usuarios.php', 'novoUsuario.php']) ? 'active' : ''; ?>" href="./usuarios.php">Usuários</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo in_array($paginaAtual, ['clientes.php', 'novoCliente.php']) ? 'active' : ''; ?>" href="./clientes.php">Clientes</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container h-75 pt-5">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./novoUsuario.php">Usuários</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./instalar.php">Instalar Banco</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./cliente.php">Cliente</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container h-75 pt-5">
+        
